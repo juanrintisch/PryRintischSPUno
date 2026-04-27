ef819cad7358b4a234e0ee17e0f1456fa1717f47
juanrintisch <juanrintisch@gmail.com>
2026-04-27 20:24:43 -0300

    bastante codigo

--- PryRintischSPUno/frmDos.cs
+++ PryRintischSPUno/frmDos.cs
@@ -62,51 +62,138 @@ namespace PryRintischSPUno
 
         private void btnAceptar_Click(object sender, EventArgs e)
         {
-
-            string Usuario = txtUsuario.Text;
-            string Contraseña = txtContraseña.Text;
-                                
-            if (Usuario == "")
+             
+            if (txtUsuario.Text == "")
             {
+                
                 MessageBox.Show("Ingrese un usuario", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 txtUsuario.Focus();
+
             }
-            else if (Contraseña == "")
+            else if (txtContraseña.Text == "")
             {
+                
                 MessageBox.Show("Ingrese una contraseña", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 txtContraseña.Focus();
+
             }
             else if (cmbModulo.SelectedIndex == -1)
             {
+                
                 MessageBox.Show("Seleccione un módulo", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 cmbModulo.Focus();
+
             }
-            else if (Usuario == "Adm" || Usuario == "Administrador" || Contraseña == "@1a" || cmbModulo.SelectedIndex == (0))
+
+            
+            if (txtUsuario.Text == "Adm" && txtContraseña.Text == "@1a" && cmbModulo.SelectedIndex == 0)
             {
                
-                DialogResult result = MessageBox.Show("Bienvenido sr/sra administrador/a", "Sintepart SRL", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                DialogResult result = MessageBox.Show("Bienvenido/a sr/sra administrador/a", "Sintepart SRL", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 this.Hide();
                 frmADM frmADM = new frmADM();
                 frmADM.Show();
+                txtUsuario.Text = ""; 
+                txtContraseña.Text = "";
 
             }
-            else if (Usuario == "Adm" || Usuario == "Administrador" || Contraseña == "@1a" || cmbModulo.SelectedIndex == (2))
+            else if (txtUsuario.Text == "Adm" && txtContraseña.Text == "@1a" && cmbModulo.SelectedIndex == 2)
             {
 
-                DialogResult result = MessageBox.Show("Bienvenido sr/sra administrador/a", "Sintepart SRL", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                DialogResult result = MessageBox.Show("Bienvenido/a sr/sra administrador/a", "Sintepart SRL", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 this.Hide();
                 frmCOM frmCOM = new frmCOM();
                 frmCOM.Show();
+                txtUsuario.Text = "";
+                txtContraseña.Text = "";
 
             }
-            else if (Usuario == "Adm" || Usuario == "Administrador" || Contraseña == "@1a" || cmbModulo.SelectedIndex == (3))
+            else if (txtUsuario.Text == "Adm" && txtContraseña.Text == "@1a" && cmbModulo.SelectedIndex == 3)
             {
 
-                DialogResult result = MessageBox.Show("Bienvenido sr/sra administrador/a", "Sintepart SRL", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                DialogResult result = MessageBox.Show("Bienvenido/a sr/sra administrador/a", "Sintepart SRL", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 this.Hide();
                 frmVTA frmVTA = new frmVTA();
                 frmVTA.Show();
+                txtUsuario.Text = "";
+                txtContraseña.Text = "";
 
+            }
+            else if (txtUsuario.Text == "Jhon" && txtContraseña.Text == "2b" && cmbModulo.SelectedIndex == 1)
+            {
+
+                DialogResult result = MessageBox.Show("Bienvenido Sr.Jhon", "Sintepart SRL", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Hide();
+                frmSIST frmSIST = new frmSIST();
+                frmSIST.Show();
+                txtUsuario.Text = "";               
+                txtContraseña.Text = "";
+
+            }
+            else if (txtUsuario.Text == "Ceci" && txtContraseña.Text == "@3c" && cmbModulo.SelectedIndex == 0)
+            {
+
+                DialogResult result = MessageBox.Show("Bienvenida Sra.Ceci", "Sintepart SRL", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Hide();
+                frmADM frmADM = new frmADM();
+                frmADM.Show();
+                txtUsuario.Text = "";
+                txtContraseña.Text = "";
+
+            }
+            else if (txtUsuario.Text == "Ceci" && txtContraseña.Text == "@3c" && cmbModulo.SelectedIndex == 3)
+            {
+                
+                DialogResult result = MessageBox.Show("Bienvenida Sra.Ceci", "Sintepart SRL", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Hide();
+                frmVTA frmVTA = new frmVTA();
+                frmVTA.Show();
+                txtUsuario.Text = "";
+                txtContraseña.Text = "";
+
+            }
+            else if (txtUsuario.Text == "God" && txtContraseña.Text == "@#4d" && cmbModulo.SelectedIndex == 0)
+            {
+                DialogResult result = MessageBox.Show("Bienvenido Sr.Dios" + "\n" + "Amèn", "Sintepart SRL", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Hide();
+                frmADM frmADM = new frmADM();
+                frmADM.Show();
+                txtUsuario.Text = "";
+                txtContraseña.Text = "";
+            }
+            else if (txtUsuario.Text == "God" && txtContraseña.Text == "@#4d" && cmbModulo.SelectedIndex == 1)
+            {
+                DialogResult result = MessageBox.Show("Bienvenido Sr.Dios" + "\n" + "Amèn", "Sintepart SRL", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Hide();
+                frmSIST frmSIST = new frmSIST();
+                frmSIST.Show();
+                txtUsuario.Text = "";
+                txtContraseña.Text = "";
+            }
+            else if (txtUsuario.Text == "God" && txtContraseña.Text == "@#4d" && cmbModulo.SelectedIndex == 2)
+            {
+                DialogResult result = MessageBox.Show("Bienvenido Sr.Dios" + "\n" + "Amèn", "Sintepart SRL", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Hide();
+                frmCOM frmCOM = new frmCOM();
+                frmCOM.Show();
+                txtUsuario.Text = "";
+                txtContraseña.Text = "";
+            }
+            else if (txtUsuario.Text == "God" && txtContraseña.Text == "@#4d" && cmbModulo.SelectedIndex == 3)
+            {
+                DialogResult result = MessageBox.Show("Bienvenido Sr.Dios" + "\n" + "Amèn", "Sintepart SRL", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Hide();
+                frmVTA frmVTA = new frmVTA();
+                frmVTA.Show();
+                txtUsuario.Text = "";
+                txtContraseña.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("Usuario y/o Contraseña Incorrectos para el modulo seleccionado", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtUsuario.Clear();
+                txtContraseña.Clear();
+                txtUsuario.Focus();
             }
         }
     }
---
+++ PryRintischSPUno/frmSIST.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PryRintischSPUno
+{
+    public partial class frmSIST : Form
+    {
+        public frmSIST()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVolver_Click(object sender, EventArgs e)
+        {
+            frmDos frmDos = new frmDos();
+            this.Hide();
+            frmDos.Show();
+        }
+    }
+}
